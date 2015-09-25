d2cde32e299eac259b0d4eecabfa215bacf2e833
Kevin R Dyke <krdyke@gmail.com>
2015-09-25 16:06:51 -0500

    scott update

--- scripts/update_scott.sh
+++ scripts/update_scott.sh
@@ -1,4 +1,4 @@
-python scrape_open_data/__init__.py "http://data.scottcounty.opendata.arcgis.com/data.json" "scott" "../scott-county"
+python scrape_open_data/__init__.py "http://data.scottcounty.opendata.arcgis.com/data.json" "scott" "../scott-county" "scrape_open_data/scott_opendata_iso_template_gmd_gone.xml"
 ../scott-county/ git iaddpw *.xml
 date_time=`date`
 git commit -m "updated scott county dataset records: $date_time"
