59f4c015d56cfe496896e268bc72522bb7e4a709
Kevin R Dyke <krdyke@gmail.com>
2015-09-28 15:44:23 -0500

    carver updates

--- scripts/update_carver.sh
+++ scripts/update_carver.sh
@@ -1,4 +1,4 @@
-python scrape_open_data/__init__.py "http://data.carver.opendata.arcgis.com/data.json" "carver" "../carver-county"
-../carver-county/ git iaddpw *.xml
-date_time=`date`
-git commit -m "updated carver county dataset records: $date_time"
+python scrape_open_data/__init__.py "http://data.carver.opendata.arcgis.com" "carver" "../carver-county" "scrape_open_data/carver_opendata_iso_template_gmd_gone.xml"
+#../carver-county/ git iaddpw *.xml
+#date_time=`date`
+#git commit -m "updated carver county dataset records: $date_time"
